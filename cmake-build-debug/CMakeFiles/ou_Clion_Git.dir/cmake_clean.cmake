b8eb1d7ffbda50a3039cc45eaf9a759d32c768c4
WiktoriaKrzyzanska <240723@edu.p.lodz.pl>
2022-01-22 18:01:05 +0100

    some init msg

---
+++ cmake-build-debug/CMakeFiles/ou_Clion_Git.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ou_Clion_Git.dir/main.cpp.o"
+  "libou_Clion_Git.dll.a"
+  "ou_Clion_Git.exe"
+  "ou_Clion_Git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ou_Clion_Git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
